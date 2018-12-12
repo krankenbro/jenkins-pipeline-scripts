0238313f179bb680508d6a8279578b0537e28a28
Alexander Morogov <asmorogov@gmail.com>
2018-12-12 17:18:35 +0200

    modules artifact dir

--- src/jobs/scripts/Modules.groovy
+++ src/jobs/scripts/Modules.groovy
@@ -87,7 +87,7 @@ class Modules {
 
             // add platform dll to test installs
         def packagesDir = Utilities.getArtifactFolder(context)
-        def allModulesDir = "c:\\Builds\\Jenkins\\VCF\\modules"
+        def allModulesDir = "c:\\CICD\\Artifacts\\"
 
         context.env.xunit_virto_modules_folder = packagesDir
         context.env.xunit_virto_dependency_modules_folder = allModulesDir
