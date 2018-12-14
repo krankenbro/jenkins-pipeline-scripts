e0b0b4dfc698a5744e5e8c1c6ea9f158fdba55b2
Alexander Morogov <asmorogov@gmail.com>
2018-12-14 11:27:57 +0200

    install module via api

--- resources/azure/vc-install-module.ps1
+++ resources/azure/vc-install-module.ps1
@@ -3,10 +3,6 @@ Param(
     $apiurl,
     [parameter(Mandatory = $true)]
     $moduleZipArchievePath,
-    [parameter(Mandatory = $true)]
-    $moduleId,
-    [parameter(Mandatory = $true)]
-    $platformContainer,
     $hmacAppId,
     $hmacSecret
 )
@@ -21,19 +17,17 @@ if ([string]::IsNullOrWhiteSpace($hmacSecret)) {
     $hmacSecret = "${env:HMAC_SECRET}"
 }
 
-Write-Output $platformContainer
-Write-Output $moduleZipArchievePath
-docker cp $moduleZipArchievePath ${platformContainer}:/vc-platform/
-docker exec $platformContainer powershell -Command "Remove-Item C:\vc-platform\Modules\$moduleId -Force -Recurse"
-docker exec $platformContainer powershell -Command "Expand-Archive -Path C:\vc-platform\*.zip -DestinationPath C:\vc-platform\Modules\$moduleId"
 
-#restart platform
+$moduleUploadUrl = "$apiurl/api/platform/modules/localstorage"
+$moduleInstallUrl = "$apiurl/api/platform/modules/install"
 $restartUrl = "$apiurl/api/platform/modules/restart"
 
 $headerValue = Create-Authorization $hmacAppId $hmacSecret
 $headers = @{}
 $headers.Add("Authorization", $headerValue)
 
-Write-Output "Restarting website"
-$moduleState = Invoke-RestMethod "$restartUrl" -Method Post -ContentType "application/json" -Headers $headers
-Write-Output $moduleState
+$moduleUploadResult = Invoke-MultipartFormDataUpload -InFile $moduleZipArchievePath -Uri $moduleUploadUrl -Authorization $headerValue
+Write-Output $moduleUploadResult
+$moduleInstallResult = Invoke-RestMethod -Uri $moduleInstallUrl -Method Post -Headers $headers -Body $moduleUploadResult
+Write-Output $moduleInstallResult
+Start-Sleep -s 3
