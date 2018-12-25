f2508958f7102968c78beb45113d1435126d477d
Alexander Morogov <asmorogov@gmail.com>
2018-12-25 17:56:08 +0200

    module installation pushnotify

--- resources/azure/vc-install-module.ps1
+++ resources/azure/vc-install-module.ps1
@@ -21,6 +21,7 @@ if ([string]::IsNullOrWhiteSpace($hmacSecret)) {
 $moduleUploadUrl = "$apiurl/api/platform/modules/localstorage"
 $moduleInstallUrl = "$apiurl/api/platform/modules/install"
 $restartUrl = "$apiurl/api/platform/modules/restart"
+$pushUrl = "$apiurl/api/platform/pushnotifications"
 
 $headerValue = Create-Authorization $hmacAppId $hmacSecret
 $headers = @{}
@@ -29,5 +30,23 @@ $headers.Add("Authorization", $headerValue)
 $moduleUploadResult = Invoke-MultipartFormDataUpload -InFile $moduleZipArchievePath -Uri $moduleUploadUrl -Authorization $headerValue
 Write-Output $moduleUploadResult
 $moduleInstallResult = Invoke-RestMethod -Uri $moduleInstallUrl -Method Post -Headers $headers -Body $moduleUploadResult
-Write-Output $moduleInstallResult
+$notificationId = $moduleInstallResult.id
+$NotificationStateJson = @"
+     {"Ids":["$notificationId"],"start":0, "count": 1}
+"@
+
+$notify = @{}
+do {
+    Start-Sleep -s 3
+    $state = Invoke-RestMethod "$pushUrl" -Body $NotificationStateJson -Method Post -ContentType "application/json" -Headers $headers
+    Write-Output $state
+    if ($state.notifyEvents -ne $null ) {
+        $notify = $state.notifyEvents
+        if ($notify.errorCount -gt 0) {
+            Write-Output $notify
+            exit 1
+        }
+    }
+}
+while (([string]::IsNullOrEmpty($notify.finished)) -and $cycleCount -lt 180)
 Start-Sleep -s 3
