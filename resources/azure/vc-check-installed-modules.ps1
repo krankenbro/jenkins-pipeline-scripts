93752a5c688dc31da63c07443563a9ee332fa67a
Alexander Morogov <asmorogov@gmail.com>
2018-12-26 11:25:54 +0200

    install module missing parameter

--- resources/azure/vc-check-installed-modules.ps1
+++ resources/azure/vc-check-installed-modules.ps1
@@ -37,6 +37,6 @@ Foreach ($module in $modules) {
     }
 }
 Write-Output "Modules installed: $installedModules"
-if($installedModules -lt 20){
+if($false -and $installedModules -lt 20){
     exit 1
 }
